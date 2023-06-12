3b6e5f4fe88bebffa54d397a2e7aea784b6be12e
PKRVT <loflinkyle@gmail.com>
2023-06-11 21:30:33 -0700

    part 1 attempt 1 for the pa

---
+++ move.py
@@ -0,0 +1,18 @@
+#Author: Kyle Loflin
+#Class: CSC 110
+#Description: This program will take in inputs from a user to make a madlib
+#list of inputs 
+r1 = input('Female name: \n')
+r2 = input('Street name: \n')
+r3 = input('Male name: \n')
+r4 = input('An object: \n')
+r5 = input('A vehicle: \n')
+r6 = input('An adjective: \n')
+#creat space
+print(' ')
+#Display the madlib
+print(r1, 'decided to move from her apartment on 5th')
+print('to a condo on', r2 + '. She called her friend', r3)
+print('for help. However, they could not fit', r4, 'into')
+print('the moving truck, so they had to rent a', r5, r6)
+print('in order to move it!')
