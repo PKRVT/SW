dfe1b1f079fd9b10a2e954c1fab00f4117e6c2df
PKRVT <loflinkyle@gmail.com>
2023-06-19 22:08:32 -0700

    ehh

--- prep10.py
+++ prep10.py
@@ -1,12 +1,13 @@
 string = input('Enter string:\n')
 i = len(string) - 1
-count = 1
-while count != 0 or count > 0 or count < 1:
+count = 0
+while count == 0:
     if string[i] == ')':
-        count += + 1
-    while count != 0:
-        if string[i] == '(':
-            count += - 1
+        count += 1
+    if string[i] == '(':
+        count += - 1
+    if count == 0:
+        exit()
 if count == 0:
     print('Parentheses balanced')
 else:
