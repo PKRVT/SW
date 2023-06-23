00daf3bbe2c11b364a2f519afbfbd34862448d67
PKRVT <loflinkyle@gmail.com>
2023-06-23 15:47:41 -0700

    input val v2

--- target_practice.py
+++ target_practice.py
@@ -1,12 +1,7 @@
-#main for the functions to connect in
-def main():
-    string1 = input('Hit string: \n')
-    string2 = input('Marker: \n')
-    input(string1, string2)
-def input(s1, s2):
-    '''this validate inputs from user'''
-    if len(s1) > 3 or len(s1) % 4 != 0:
-        print('Please provide a valid hit string.')
-    if len(s2) != 1:
-        print('Please provide a valid marker.')
-main()
+#get and validate inputs
+string1 = input('Hit string: \n')
+if len(string1) > 3 or len(string1) % 4 != 0:
+    print('Please provide a valid hit string.')
+string2 = input('Marker: \n')
+if len(string2) != 1:
+    print('Please provide a valid marker')
